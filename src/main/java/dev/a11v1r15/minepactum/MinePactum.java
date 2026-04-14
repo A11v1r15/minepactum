da51842d628aed9f150eb5db9a81b1d39231b1c2
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2026-04-13 22:07:51 -0300

    ✨ Stamp item wo function

--- src/main/java/dev/a11v1r15/minepactum/MinePactum.java
+++ src/main/java/dev/a11v1r15/minepactum/MinePactum.java
@@ -1,6 +1,7 @@
 package dev.a11v1r15.minepactum;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,11 @@ public class MinePactum implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		MinePactumItems.initialize();
 		LOGGER.info("Pacta sunt servanda");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(ID, path);
 	}
 }
---
+++ src/main/java/dev/a11v1r15/minepactum/MinePactumItems.java
@@ -0,0 +1,57 @@
+package dev.a11v1r15.minepactum;
+
+import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab;
+import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.*;
+
+import java.util.function.Function;
+
+public class MinePactumItems {
+	public static <T extends Item> T register(String name, Function<Item.Properties, T> itemFactory, Item.Properties settings) {
+		ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, MinePactum.id(name));
+		T item = itemFactory.apply(settings.setId(itemKey));
+		Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+
+		return item;
+	}
+	public static final ResourceKey<CreativeModeTab> MINEPACTUM_TAB_KEY = ResourceKey.create(
+		BuiltInRegistries.CREATIVE_MODE_TAB.key(), MinePactum.id("minepactum_tab")
+	);
+	public static final CreativeModeTab MINEPACTUM_TAB = FabricCreativeModeTab.builder()
+		.icon(() -> new ItemStack(MinePactumItems.STAMP))
+		.title(Component.translatable("creativeTab.minepactum"))
+		.displayItems((params, output) -> {
+			output.accept(Items.WRITABLE_BOOK);
+			output.accept(MinePactumItems.STAMP);
+		})
+		.build();
+
+	public static final Item STAMP = register("stamp", Item::new,
+		new Item.Properties()
+			.durability(30)
+			.stacksTo(1)
+			.repairable(Items.LAPIS_LAZULI)
+	);
+	public static final WrittenBookItem PACT = register("pact", WrittenBookItem::new,
+		new Item.Properties()
+			.stacksTo(16)
+	);
+	public static final WrittenBookItem SEALED_PACT = register("sealed_pact", WrittenBookItem::new,
+		new Item.Properties()
+			.stacksTo(16)
+	);
+	public static final WrittenBookItem OFFICIAL_PACT = register("official_pact", WrittenBookItem::new,
+		new Item.Properties()
+			.stacksTo(16)
+	);
+
+
+	public static void initialize() {
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, MINEPACTUM_TAB_KEY, MINEPACTUM_TAB);
+	}
+}
